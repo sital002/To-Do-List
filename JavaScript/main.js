f36b4197b51b2bae9a2d531ca7ea51529cf81a1d
sital002 <sitaladhikari002@gmail.com>
2022-04-28 14:26:45 +0545

    to do list project....

---
+++ JavaScript/main.js
@@ -0,0 +1,57 @@
+
+// let current_tasks=document.querySelectorAll(".delete");
+let addBtn=document.querySelector("#push");
+let inputField=document.querySelector("#new-task input");    
+chkEnter();
+addBtn.onclick=function(){
+    CreateTask();
+    ChkTaskstats();
+   
+}
+
+//Checking the whether the enter key is pressed or not.
+
+
+function chkEnter(){
+   inputField.addEventListener("keypress",(event)=>{
+        if(event.key=='Enter'){
+            console.log("Task Added");
+            CreateTask();
+            ChkTaskstats();
+    }
+});
+}
+function CreateTask(){
+    if(inputField.value.length==0){
+        alert("Enter a Task");
+        }
+        else{
+
+            document.querySelector("#tasks").innerHTML+= `
+            <div class="tasks">
+            <span class="taskname">
+            ${inputField.value}
+            </span>
+            <button class="delete">DELETE</button>
+            </div>
+            `
+            inputField.value="";
+            inputField.focus();
+        }
+    }
+    function ChkTaskstats(){
+        let current_tasks=document.querySelectorAll(".delete");
+        let tasks=document.querySelectorAll(".taskname");
+        for(i=0;i<current_tasks.length;i++){
+            current_tasks[i].onclick=function(){
+                this.parentNode.remove();
+            } }
+            for(i=0;i<tasks.length;i++){
+                tasks[i].onclick=function(){
+                    this.classList.toggle("completed");
+                }
+            }
+    }
+
+   
+    
